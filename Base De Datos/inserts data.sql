4f10957d9fbb8da8c0c2388f644f33b64d3a9e8e
cocobs <tomasjuarez2005@gmail.com>
2023-09-05 15:53:30 -0300

    Create inserts data.sql

---
+++ Base De Datos/inserts data.sql
@@ -0,0 +1,29 @@
+INSERT INTO ESTADISTICAS tmp/hum VALUES
+('21/06/23',14,18,30),
+('23/06/23',16,19,45),
+('25/06/23',12,34,37),
+('27/06/23',14,40,42);
+
+INSERT INTO PRECIPITACIONES VALUES 
+('21/06/23', 15,'15:30'),
+('23/06/23', 17,'17:40'),
+('25/06/23', 12,'12:50'),
+('27/06/23', 13,'13:25');
+
+INSERT into intensidad solar VALUES
+('21/06/23',12,20,10),
+('23/06/23',14,17,11),
+('25/06/23',16,18,13),
+('27/06/23',18,20,9);
+
+INSERT into presion atmosferica values 
+('21/06/23',2,),
+('23/06/23',0,89,),
+('25/06/23',0,87,),
+('17/06/23',0,99,),
+
+INSERT into domicilio values 
+('buenos aires','la plata'),
+('santiago del estero','santiago del estero'),
+('chubut','rawson'),
+('chaco','resistencia');
